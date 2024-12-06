2c578a50de58f7faaa3c04c1f83c2b6cf3e04f25
helauren-laptop <helauren42@gmail.com>
2024-12-06 04:02:49 +0100

    added queen obstacle check

--- INC/Board.hpp
+++ INC/Board.hpp
@@ -86,8 +86,20 @@ class Board {
 					}
 					return false;
 				}
-				case QUEEN:
+				case QUEEN: {
+					short incrX = move.x > 0 ? 1 : -1;
+					short incrY = move.y > 0 ? 1 : -1;
+					while((old_pos.x != new_pos.x || old_pos.y != new_pos.y)) {
+						if(old_pos.x != new_pos.x)
+							old_pos.x += incrX;
+						if(old_pos.y != new_pos.y)
+							old_pos.y += incrY;
+						if(board[old_pos.y][old_pos.x].type != NONE) {
+							return true;
+						}
+					}
 					return false;
+				}
 				case KING:
 					return false;
 				default:
