b59824cd9f84c2e4ac2e2b650e0f4c835c6dfe10
helauren-laptop <helauren42@gmail.com>
2024-12-12 13:46:47 +0100

    remove SDL

--- INC/Game.hpp
+++ INC/Game.hpp
@@ -1,9 +1,6 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
 #include "FrontEnd.hpp"
 #include "Pieces.hpp"
 #include "Board.hpp"
@@ -11,50 +8,6 @@
 // #include "Player.hpp"
 
 #include "../INC/MyCppLib/MyCppLib.hpp"
-
-SDL_Window *initWindow(t_dim &dim)
-{
-	SDL_DisplayMode display_mode;
-	int screen_width = 0;
-	int screen_height = 0;
-
-	SDL_Window *window = NULL;
-
-	if (SDL_GetDesktopDisplayMode(0, &display_mode) != 0)
-	{
-		printf("failed to get display mode: %s\n", SDL_GetError());
-		return NULL;
-	}
-	screen_width = display_mode.w;
-	screen_height = display_mode.h;
-	
-	window = SDL_CreateWindow(
-		"SDL Example",
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		screen_width, screen_height,
-		SDL_WINDOW_FULLSCREEN);
-
-	if (!window)
-		return NULL;
-
-	SDL_SetWindowFullscreen(window, 0);
-
-	SDL_GetWindowSize(window, &dim.window_width, &dim.window_height);
-
-	int top = 0, left = 0, bottom = 0, right = 0;
-	if (SDL_GetWindowBordersSize(window, &top, &left, &bottom, &right) != 0)
-	{
-		printf("Failed to get window borders: %s", SDL_GetError());
-		return NULL;
-	}
-
-	dim.window_height = screen_height - top - bottom;
-	dim.window_width = screen_width - left - right;
-
-	dim.board = dim.window_height * 0.95;
-	dim.square = dim.board / 8;
-	return window;
-}
 
 class Game
 {
@@ -64,8 +17,6 @@ private:
 	WINNER winner = COLOR_NONE;
 	Board board;
 	t_dim dim;
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
 	// Player player;
 public:
 	Game() { board.setPlayerTurn(&player_turn); };
@@ -96,13 +47,11 @@ public:
 
 	void run()
 	{
-		SDL_Event e;
 		bool quit = false;
 		bool moved = false;
 		bool game_end = false;
 		while (!quit)
 		{
-			SDL_Rect rect = {0, 0, dim.board, dim.board};
 			// while (SDL_PollEvent(&e) != 0)
 			// {
 			// 	events.eventHandler(e, player_turn, quit);
@@ -127,7 +76,6 @@ public:
 				}
 				board.setMoved(false);
 			}
-			SDL_Delay(64);
 		}
 		if (game_end)
 		{
@@ -138,13 +86,11 @@ public:
 				out("BLACK\n");
 			else
 				out("STALEMATE\n");
-			SDL_Delay(640);
 		}
 	}
 
 	void destroy()
 	{
-		freeing(renderer, window);
 		close(logger_fd);
 	}
 };
