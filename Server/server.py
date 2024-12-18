d2672797a4ae1233acf768734d3bc16d04318244
helauren-laptop <helauren42@gmail.com>
2024-12-18 19:52:40 +0100

    added accounts to db

--- Server/server.py
+++ Server/server.py
@@ -4,15 +4,24 @@ import fastapi
 connect = sqlite3.connect("database.db")
 cursor = connect.cursor()
 
+cursor.execute("CREATE TABLE IF NOT EXISTS account (\
+				id INTEGER PRIMARY KEY AUTOINCREMENT, \
+				username CHAR(25) NOT NULL,\
+				password CHAR(25) NOT NULL, \
+			dob TEXT NOT NULL)")
+
 app = fastapi.FastAPI()
 
 @app.get("/")
 async def home(request: fastapi.Request):
 	return {"message": "GET Hello World Your Home Baby"}
 
+@app.get("/login")
+async def home(request: fastapi.Request):
+	return {"message": "GET Hello World Your Home Baby"}
+
 @app.post("/signup")
 async def signup(request: fastapi.Request):
-	print(request.headers)
 	try:
 		body = await request.json()
 		print("body: ", body)
@@ -23,5 +32,12 @@ async def signup(request: fastapi.Request):
 		print(e)
 		return {"message": "Invalid request"}
 
-	print("message: ", "Signup successful")
-	return {"message": "Signup successful"}
+	try:
+		cursor.execute("SELECT COUNT(*) FROM account")
+		result = cursor.fetchone()
+		id = str(result[0])
+		cursor.execute("INSERT INTO account (id, username, password, dob) VALUES (?, ?, ?, ?)", (id, username, password, dob))
+		return {"id": f"{id}", "message": "Signup successful"}
+	except Exception as e:
+		print("!!!!", e)
+		return {"message": "Signup failed"}
