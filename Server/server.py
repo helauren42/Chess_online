eb5a111b755e3b5860482c1d9aa2bb977647fce1
helauren-laptop <helauren42@gmail.com>
2024-12-18 11:38:47 +0100

    made draft client Poco work

--- Server/server.py
+++ Server/server.py
@@ -1,11 +1,20 @@
+import sqlite3
 import fastapi
-import logging
 
-logging.basicConfig(level=logging.DEBUG)
+connect = sqlite3.connect("database.db")
+cursor = connect.cursor()
 
 app = fastapi.FastAPI()
 
-@app.post("/signup")
-def index():
-	return {"message": "Hello World"}
+@app.get("/")
+async def home(request: fastapi.Request):
+	return {"message": "Hello World Your Home Baby"}
 
+@app.post("/signup")
+async def signup(request: fastapi.Request):
+	body = await request.json()
+	username = body.get("username")
+	password = body.get("password")
+	dob = body.get("dob")
+	print("message: ", "Signup successful")
+	return {"message": "Signup successful"}
