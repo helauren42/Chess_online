2605e5ac50bb94f267e6041a87f8f594624a8fd8
helauren-laptop <helauren42@gmail.com>
2024-12-19 13:32:46 +0100

    added manageDb

---
+++ Server/manageDb.py
@@ -0,0 +1,35 @@
+import sqlite3
+import fastapi
+import sys
+
+connect = sqlite3.connect("database.db")
+cursor = connect.cursor()
+
+ERROR = "Provide an argument, format: ./script.py accounts / online"
+
+# def listOnline():
+
+
+def listDb():
+	cursor.execute("SELECT * FROM account")
+	print("\nACCOUNTS:\n")
+	all = cursor.fetchall()
+	for account in all:
+		print(account)
+
+def main():
+	if len(sys.argv) != 2:
+		print(ERROR)
+		sys.exit(1)
+	arg = sys.argv[1]
+	match arg:
+		case "accounts":
+			listDb()
+		# case "online":
+			# listOnline()
+		case _:
+			print(ERROR)
+			sys.exit(1)
+
+if __name__ == "__main__":
+	main()
