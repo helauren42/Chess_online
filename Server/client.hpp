a8d80bddcda58244d8d7ca8574aa6632c6daf8ca
helauren-laptop <helauren42@gmail.com>
2024-12-19 13:08:05 +0100

    feat! login and signup working, succesfull login is followed by crash on occasion

--- Server/client.hpp
+++ Server/client.hpp
@@ -1,119 +1,119 @@
-#include <Poco/Net/HTTPClientSession.h>
-#include <Poco/Net/HTTPMessage.h>
-#include <Poco/Net/HTTPRequest.h>
-#include <Poco/Net/HTTPResponse.h>
-#include <Poco/URI.h>
-#include <iostream>
-#include <sstream>
+// #include <Poco/Net/HTTPClientSession.h>
+// #include <Poco/Net/HTTPMessage.h>
+// #include <Poco/Net/HTTPRequest.h>
+// #include <Poco/Net/HTTPResponse.h>
+// #include <Poco/URI.h>
+// #include <iostream>
+// #include <sstream>
 
-#include "../FrontEnd/common.hpp"
+// #include "../FrontEnd/common.hpp"
 
-using namespace Poco::Net;
-using namespace Poco;
-using namespace std;
+// using namespace Poco::Net;
+// using namespace Poco;
+// using namespace std;
 
-#define MY_URI "http://127.0.0.1:8000"
-#define PORT 8000
+// #define MY_URI "http://127.0.0.1:8000"
+// #define PORT 8000
 
-struct Date {
-	int day;
-	int month;
-	int year;
-	Date() {};
-	Date(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {};
-};
+// struct Date {
+// 	int day;
+// 	int month;
+// 	int year;
+// 	Date() {};
+// 	Date(int _day, int _month, int _year) : day(_day), month(_month), year(_year) {};
+// };
 
-struct GameInfo {
-	GameMode mode;
-	std::string opponent;
-	void set(const GameMode &_mode, const std::string _opponent = "") {
-		mode = _mode;
-		opponent = _opponent;
-	};
-};
+// struct GameInfo {
+// 	GameMode mode;
+// 	std::string opponent;
+// 	void set(const GameMode &_mode, const std::string _opponent = "") {
+// 		mode = _mode;
+// 		opponent = _opponent;
+// 	};
+// };
 
-struct Account {
-	std::string username;
-	std::string password;
-	Date dob;
-	Account	() {};
-	Account(const std::string& _username, const std::string& _password) : username(_username), password(_password) {};
-	Account(const std::string& _username, const std::string& _password, const Date& _dob) : username(_username), password(_password), dob(_dob) {};
-};
+// struct Account {
+// 	std::string username;
+// 	std::string password;
+// 	Date dob;
+// 	Account	() {};
+// 	Account(const std::string& _username, const std::string& _password) : username(_username), password(_password) {};
+// 	Account(const std::string& _username, const std::string& _password, const Date& _dob) : username(_username), password(_password), dob(_dob) {};
+// };
 
-class Online{
-private:
-	bool loggedIn = false;
-	Account account;
-	GameInfo gameInfo;
-	URI uri;
-	HTTPClientSession session;
-	HTTPRequest request;
-	HTTPResponse response;
+// class Online{
+// private:
+// 	bool loggedIn = false;
+// 	Account account;
+// 	GameInfo gameInfo;
+// 	URI uri;
+// 	HTTPClientSession session;
+// 	HTTPRequest request;
+// 	HTTPResponse response;
 
-public:
-	void makeRequests(std::string method, std::string path, std::string body) {
-		request = HTTPRequest(method, path, HTTPMessage::HTTP_1_1);
-		request.setContentType("application/json");
-        request.setContentLength(body.length());
-		std::ostream& os = session.sendRequest(request);
-		os << body;
-		os.flush();
-	}
-	Online() : uri(MY_URI) , session (uri.getHost(), uri.getPort()) {
-		session.setKeepAlive(true);
-	};
-	~Online() {}
-	std::string recvResponse() {
-		istream& rs = session.receiveResponse(response);
-		std::stringstream ss;
-		ss << rs.rdbuf();
-		return ss.str();
-	}
-	int getStatus() const {
-		return response.getStatus();
-	}
+// public:
+// 	void makeRequests(std::string method, std::string path, std::string body) {
+// 		request = HTTPRequest(method, path, HTTPMessage::HTTP_1_1);
+// 		request.setContentType("application/json");
+//         request.setContentLength(body.length());
+// 		std::ostream& os = session.sendRequest(request);
+// 		os << body;
+// 		os.flush();
+// 	}
+// 	Online() : uri(MY_URI) , session (uri.getHost(), uri.getPort()) {
+// 		session.setKeepAlive(true);
+// 	};
+// 	~Online() {}
+// 	std::string recvResponse() {
+// 		istream& rs = session.receiveResponse(response);
+// 		std::stringstream ss;
+// 		ss << rs.rdbuf();
+// 		return ss.str();
+// 	}
+// 	int getStatus() const {
+// 		return response.getStatus();
+// 	}
 
-	void login(const std::string& username, const std::string& password) {}
-	std::string accountJson(const std::string& username, const std::string& password, const Date& dob) {
-		std::string ret;
-		ret = std::string("{") + "\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"dob\":\"" + to_string(dob.day) + "/" + to_string(dob.month) + "/" + to_string(dob.year) + "\"}";
-		return ret;
-	}
-	std::pair<int, std::string> createAccount(const std::string& username, const std::string& password, const Date& dob) {
-		makeRequests(HTTPRequest::HTTP_POST,"/signup", accountJson(username, password, dob));
-		return {getStatus(), recvResponse()};
-	}
-	// void logIn() { loggedIn = true; };
-	// void logOut() { loggedIn = false; };
-	bool getLoggedIn() const { return loggedIn; };
-};
+// 	void login(const std::string& username, const std::string& password) {}
+// 	std::string accountJson(const std::string& username, const std::string& password, const Date& dob) {
+// 		std::string ret;
+// 		ret = std::string("{") + "\"username\":\"" + username + "\",\"password\":\"" + password + "\",\"dob\":\"" + to_string(dob.day) + "/" + to_string(dob.month) + "/" + to_string(dob.year) + "\"}";
+// 		return ret;
+// 	}
+// 	std::pair<int, std::string> createAccount(const std::string& username, const std::string& password, const Date& dob) {
+// 		makeRequests(HTTPRequest::HTTP_POST,"/signup", accountJson(username, password, dob));
+// 		return {getStatus(), recvResponse()};
+// 	}
+// 	// void logIn() { loggedIn = true; };
+// 	// void logOut() { loggedIn = false; };
+// 	bool getLoggedIn() const { return loggedIn; };
+// };
 
-// int main() {
-//     try {
+// // int main() {
+// //     try {
 
-//         URI uri("http://127.0.0.1:8000/");
-//         HTTPClientSession session(uri.getHost(), uri.getPort());
+// //         URI uri("http://127.0.0.1:8000/");
+// //         HTTPClientSession session(uri.getHost(), uri.getPort());
 
-//         cout << "get path: " << uri.getPath() << endl;
+// //         cout << "get path: " << uri.getPath() << endl;
 
-//         HTTPRequest request(HTTPRequest::HTTP_GET, uri.getPath(), HTTPMessage::HTTP_1_1);
+// //         HTTPRequest request(HTTPRequest::HTTP_GET, uri.getPath(), HTTPMessage::HTTP_1_1);
 
-//         session.sendRequest(request);
+// //         session.sendRequest(request);
 
-//         HTTPResponse response;
-//         istream& rs = session.receiveResponse(response);
+// //         HTTPResponse response;
+// //         istream& rs = session.receiveResponse(response);
 
-//         cout << "HTTP Status: " << response.getStatus() << rs.rdbuf() << endl;
+// //         cout << "HTTP Status: " << response.getStatus() << rs.rdbuf() << endl;
 
-//         stringstream ss;
-//         ss << rs.rdbuf();
-//         cout << "Response body:\n" << ss.str() << endl;
-//     }
-//     catch (const Exception& exc) {
-//         cerr << "Error: " << exc.displayText() << endl;
-//         return 1;
-//     }
+// //         stringstream ss;
+// //         ss << rs.rdbuf();
+// //         cout << "Response body:\n" << ss.str() << endl;
+// //     }
+// //     catch (const Exception& exc) {
+// //         cerr << "Error: " << exc.displayText() << endl;
+// //         return 1;
+// //     }
 
-//     return 0;
-// }
+// //     return 0;
+// // }
--- Server/server.py
+++ Server/server.py
@@ -1,10 +1,13 @@
 import sqlite3
 import fastapi
+from pydantic import BaseModel
+from fastapi import Response
+from fastapi.responses import JSONResponse
 
 connect = sqlite3.connect("database.db")
 cursor = connect.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS account (\
+cursor.execute("CREATE TABLE IF NOT EXISTS account ( \
 				id INTEGER PRIMARY KEY AUTOINCREMENT, \
 				username CHAR(25) NOT NULL,\
 				password CHAR(25) NOT NULL, \
@@ -12,13 +15,53 @@ cursor.execute("CREATE TABLE IF NOT EXISTS account (\
 
 app = fastapi.FastAPI()
 
+class LoginRequest():
+	def validPassword(search : str, id : int) -> bool:
+		print("valid password")
+		cursor.execute("SELECT password FROM account")
+		passwords = cursor.fetchall()
+		if(passwords[id -1][0] == search):
+			return True
+
+		return False
+	def findUserId(search : str) -> int:
+		print("find user id")
+		cursor.execute("SELECT username FROM account")
+		usernames = cursor.fetchall()
+		id:int = 1
+		for user in usernames:
+			if(user[0] == search):
+				print("found: ", user[0])
+				return id
+			id += 1
+		return -1
+
 @app.get("/")
 async def home(request: fastapi.Request):
 	return {"message": "GET Hello World Your Home Baby"}
 
-@app.get("/login")
-async def home(request: fastapi.Request):
-	return {"message": "GET Hello World Your Home Baby"}
+@app.post("/login")
+async def home(request: fastapi.Request, response: Response):
+
+	body = await request.json()
+	print("!!!BODY: ", body)
+	username = body.get("username")
+	id = LoginRequest.findUserId(username) # verifies user is in db and retrieves his id
+	print("id: ", id)
+	if(id < 0):
+		return JSONResponse(
+			status_code=401,
+			content={"message": "Login failed username"}
+		)
+
+	password = body.get("password")
+	if not LoginRequest.validPassword(password, id):
+		return JSONResponse(
+			status_code=401,
+			content={"message": "Login failed password"}
+		)
+
+	return {"message": "Login successful"}
 
 @app.post("/signup")
 async def signup(request: fastapi.Request):
@@ -30,14 +73,25 @@ async def signup(request: fastapi.Request):
 		dob = body.get("dob")
 	except Exception as e:
 		print(e)
-		return {"message": "Invalid request"}
+		return {"message": "Invalid signup request"}
 
 	try:
 		cursor.execute("SELECT COUNT(*) FROM account")
 		result = cursor.fetchone()
 		id = str(result[0])
 		cursor.execute("INSERT INTO account (id, username, password, dob) VALUES (?, ?, ?, ?)", (id, username, password, dob))
+		connect.commit()
+		print("id", f"{id}", "message", "Signup successful")
 		return {"id": f"{id}", "message": "Signup successful"}
 	except Exception as e:
 		print("!!!!", e)
 		return {"message": "Signup failed"}
+
+# @app.delete("delete_account")
+# async def delete_account(request: fastapi.Request):
+# 	try:
+# 		body = await request.json()
+# 		#  use id to delete account I guess
+# 	except Exception as e:
+# 		print(e)
+# 		return {"message": "Invalid request"}
