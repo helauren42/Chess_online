7cdcb89a7460c2cb080ec0a1b116c28c469e0717
helauren-laptop <helauren42@gmail.com>
2024-12-20 07:48:22 +0100

    push executable

--- Qt/build/Desktop_Qt_6_8_1-Release/ui_menu.h
+++ Qt/build/Desktop_Qt_6_8_1-Release/ui_menu.h
@@ -42,16 +42,16 @@ public:
         AI->setGeometry(QRect(380, 190, 151, 25));
         Llocal = new QLabel(Menu);
         Llocal->setObjectName("Llocal");
-        Llocal->setGeometry(QRect(300, 120, 41, 31));
+        Llocal->setGeometry(QRect(290, 120, 41, 31));
         Lonline = new QLabel(Menu);
         Lonline->setObjectName("Lonline");
-        Lonline->setGeometry(QRect(300, 330, 71, 31));
+        Lonline->setGeometry(QRect(290, 330, 71, 31));
         pushButton_3 = new QPushButton(Menu);
         pushButton_3->setObjectName("pushButton_3");
-        pushButton_3->setGeometry(QRect(390, 390, 151, 25));
+        pushButton_3->setGeometry(QRect(370, 390, 151, 25));
         lineEdit = new QLineEdit(Menu);
         lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(120, 390, 141, 25));
+        lineEdit->setGeometry(QRect(100, 390, 141, 25));
         line = new QFrame(Menu);
         line->setObjectName("line");
         line->setGeometry(QRect(0, 70, 641, 20));
@@ -67,7 +67,7 @@ public:
         hotseat->setGeometry(QRect(110, 190, 151, 25));
         logOut = new QPushButton(Menu);
         logOut->setObjectName("logOut");
-        logOut->setGeometry(QRect(20, 30, 101, 25));
+        logOut->setGeometry(QRect(560, 30, 61, 25));
 
         retranslateUi(Menu);
 
