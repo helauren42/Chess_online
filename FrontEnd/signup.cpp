549664b3bac2cb31a0cc2a1efdb9c08ad124741e
helauren-laptop <helauren42@gmail.com>
2025-01-05 10:41:12 +0100

    before I switch from http login to websocket login

--- FrontEnd/signup.cpp
+++ FrontEnd/signup.cpp
@@ -16,10 +16,12 @@ signup::~signup()
 void signup::setState() {
 	QPalette palette = this->ui->state->palette();
 	palette.setColor(QPalette::WindowText, Qt::red);
-	if(state_message == "") {
+	qDebug() << "sign up state msg1:" << state_message;
+	if(this->state_message == QString("")) {
         palette.setColor(QPalette::WindowText, Qt::green);
-		state_message == "Account created successfully!";
+		this->state_message = "Account created successfully!";
 	}
+	qDebug() << "sign up state msg2:" << state_message;
 	
 	this->ui->state->setPalette(palette);
 	this->ui->state->setText(this->state_message);
